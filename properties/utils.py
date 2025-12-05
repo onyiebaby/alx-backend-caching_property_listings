610bad335ba85d977b2f7b735fd745e6ae80269c
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-12-05 02:23:37 +0100

    Deployment

--- properties/utils.py
+++ properties/utils.py
@@ -1,1 +1,4 @@
 def get_all_properties(): "", Property.objects.all(), cache.get, all_properties, 3600, get_all_properties
+def get_redis_cache_metrics(): "", keyspace_hits, keyspace_misses
+if total_requests > 0 else 0
+return logger.error

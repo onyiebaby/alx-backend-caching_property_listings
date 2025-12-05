8e91a91cebc44c7cee57a4daaa71f5f6ad222a82
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-12-05 01:48:32 +0100

    Deployment

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
 # Create your views here.
+
+def property_list(request): , @cache_page(60 * 15)
+return JsonResponse({"", "properties", "data"})
